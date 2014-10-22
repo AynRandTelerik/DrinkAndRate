0b4139cee69ea7eaeab1b4292013f6687ecc315d
Lyubomir Ganchev <luboganchev91@gmail.com>
2014-10-22 02:21:10 -0700

    User changes fix

--- DrinkAndRate.Web/Account/Register.aspx.cs
+++ DrinkAndRate.Web/Account/Register.aspx.cs
@@ -6,15 +6,61 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Owin;
 using DrinkAndRate.Models;
+using DrinkAndRate.Data;
 
 namespace DrinkAndRate.Web.Account
 {
     public partial class Register : Page
     {
+        private IDrinkAndRateData data;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var dbContext = new DrinkAndRateDbContext();
+            this.data = new DrinkAndRateData(dbContext);
+
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
         protected void CreateUser_Click(object sender, EventArgs e)
         {
             var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
-            var user = new AppUser() { UserName = Email.Text, Email = Email.Text };
+            var user = new AppUser()
+            {
+                UserName = Email.Text,
+                Email = Email.Text,
+                FirstName = FirstName.Text,
+                LastName = LastName.Text,
+                CountryID = int.Parse(Countries.SelectedValue)
+            };
+
+            string filePathAndName = string.Empty;
+
+            try
+            {
+                filePathAndName = FileUploadControl.Upload();
+            }
+            catch (InvalidOperationException ex)
+            {
+                this.DivLabelErrorMessage.Visible = true;
+                this.LabelErrorMessage.Text = ex.Message;
+
+                return;
+            }
+
+            if (string.IsNullOrEmpty(filePathAndName))
+            {
+                filePathAndName = "~/Images/default.png";
+            }
+
+            user.Image = new Image
+            {
+                Path = filePathAndName
+            };
+
             IdentityResult result = manager.Create(user, Password.Text);
             if (result.Succeeded)
             {
@@ -26,10 +72,17 @@ namespace DrinkAndRate.Web.Account
                 IdentityHelper.SignIn(manager, user, isPersistent: false);
                 IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
             }
-            else 
+            else
             {
                 ErrorMessage.Text = result.Errors.FirstOrDefault();
             }
+        }
+
+        private void LoadData()
+        {
+            var allCountries = this.data.Countries.All().ToList();
+            this.Countries.DataSource = allCountries;
+            this.Countries.DataBind();
         }
     }
 }
